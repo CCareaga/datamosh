7ed469bf1cccd901eb4ef568d372c80339bd27d0
maheshkkumar <maheshk2194@gmail.com>
2021-04-18 19:27:29 -0700

    Refactored code

--- mosh_utils.py
+++ mosh_utils.py
@@ -18,33 +18,6 @@ def get_temp_name(path: str) -> str:
     base = os.path.splitext(path)[0]
     return f'{base}_tmp.avi'
 
-def resize_image(image, max_size, align=1, suppress_messages=False):
-    if not suppress_messages:
-        print("Original size: %d x %d" % (image.shape[1], image.shape[0]))
-
-    image = np.float32(image) / 255.0
-
-    H, W = image.shape[:2]
-    long_side = float(max(W, H))
-    scale = min(1.0, max_size / long_side)
-    resized_height = int(H * scale)
-    resized_width = int(W * scale)
-    if resized_width % align != 0:
-        resized_width = align * round(resized_width / align)
-        if not suppress_messages:
-            print("Rounding width to closest multiple of %d." % align)
-    if resized_height % align != 0:
-        resized_height = align * round(resized_height / align)
-        if not suppress_messages:
-            print("Rounding height to closest multiple of %d." % align)
-
-    if not suppress_messages:
-        print("Resized: %d x %d" % (resized_width, resized_height))
-    image = cv2.resize(
-        image, (resized_width, resized_height), interpolation=cv2.INTER_AREA
-    )
-    return image
-
 
 def read_frames(fname: str, h: int = 720, w: int = 1080) -> list:
     """Method to read frames from a video path
